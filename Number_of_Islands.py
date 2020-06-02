b8efcd3e82c3481c5c782e441e1ea6afab8347fb
jadewu <jadewunue@gmail.com>
2020-06-02 16:53:15 -0400

    Create Number_of_Islands.py

---
+++ Number_of_Islands.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        m = len(grid)
+        n = len(grid[0])
+        res = 0
+        def dfs(i,j):
+            if i<0 or i>m-1 or j<0 or j>n-1 or grid[i][j] != '1':
+                return
+            grid[i][j] = '#'
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    res += 1
+                    dfs(i,j)
+        return res

13c35953e659ccfa2b70a643c278456947f128c7
Jade Wu <jadewunue@gmail.com>
2020-06-09 09:40:45 -0400

    Create Implement_Stack_using_Queues.py

---
+++ Implement_Stack_using_Queues.py
@@ -0,0 +1,45 @@
+# 直接在push的时候把整个queue倒过来，变成stack的顺序，这样top就好写了，注意要用deque做
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q = collections.deque()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        s = self.q
+        s.append(x)
+        for _ in range(len(s)-1):
+            s.append(s.popleft())
+            
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.q.popleft()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.q[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        if not self.q:
+            return True
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()

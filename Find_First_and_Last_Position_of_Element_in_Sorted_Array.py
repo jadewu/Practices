7d81468e9c277b2c53ab3e94a92ccc66493d806d
Jade Wu <jadewunue@gmail.com>
2020-07-26 08:35:55 -0400

    Update Find_First_and_Last_Position_of_Element_in_Sorted_Array.py

--- Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -2,25 +2,28 @@
 # 一定要是left<=right，每次二分，移到mid + 1或mid - 1的位置
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if target not in nums:
+#         开头不可以这样写，因为in/ not in相当于是做了一遍遍历，O(n)
+#         if target not in nums:
+#             return [-1, -1]
+        i, j = 0, len(nums)-1
+        res = [-1, -1]
+        if nums == []:
             return [-1, -1]
-        res = [0, 0]
-        left = 0
-        right = len(nums)-1
-        while left <= right:
-            mid = int((left+right)/2)
+        while i <= j:
+            mid = (i+j) //2
+            if nums[mid] >= target:
+                j = mid - 1
+            else:
+                i = mid + 1
+        if (0 <= i <= len(nums)-1) and (nums[i] == target): 
+            res[0] = i
+        i, j = 0, len(nums)-1
+        while i <= j:
+            mid = (i+j) //2
             if nums[mid] <= target:
-                left = mid + 1
+                i = mid + 1
             else:
-                right = mid - 1
-        res[1] = right
-        left = 0
-        right = len(nums)-1
-        while left <= right:
-            mid = int((left+right)/2)
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        res[0] = left
-        return res   
+                j = mid - 1
+        if (0 <= j <= len(nums)-1) and (nums[j] == target):
+            res[1] = j
+        return res  

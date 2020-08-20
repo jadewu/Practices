02d139fa8a0ef55b7dcbaae76a6d976ef6accd23
Jade Wu <jadewunue@gmail.com>
2020-08-20 10:46:48 -0400

    Update Find_Minimum in_Rotated_Sorted_Array.py

--- Find_Minimum in_Rotated_Sorted_Array.py
+++ Find_Minimum in_Rotated_Sorted_Array.py
@@ -1,5 +1,7 @@
 # The list is in ascending order but moving left part to the front like (4,5,1,2,3)
 # Binary search is efficient in finding an element in sorted list
+# 只需要比较mid和r的大小，因为发生旋转，一定是从右边挪到左边，所以旋转后的最右一定比最左小
+# 如果旋转后的最右比mid小，那么最小值一定在mid右边；相反，最小值在mid左边
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums)-1

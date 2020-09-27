3ebdc6b22646b0c152296794b5176027d530d522
Jade Wu <jadewunue@gmail.com>
2020-09-26 22:17:09 -0400

    Update First_Missing_Positive.py

--- First_Missing_Positive.py
+++ First_Missing_Positive.py
@@ -12,6 +12,10 @@ class Solution:
         return last
 
 # 答案，O(n)
+# 遍历第一遍，把nums里面小于等于0和大于n的项都改为1
+# 遍历第二遍，把第abs(nums[i])项的符号变为负，用来表示正整数abs(nums[i])已经出现了，如果nums[i]==n，就把第0项变为负
+# 遍历第三遍，从1到n-1，找到第一个符号不为负的数，是第i项，那么表示正整数i并未在数列里出现过
+# 如果1到n-1项都为负且第0项为正，那么最小正整数是n
 class Solution:
     def firstMissingPositive(self, nums):
         """
